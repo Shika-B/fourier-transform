65e19fe93f91c979ea312421485d29e79ddb01e1
Shika-B <93951827+Shika-B@users.noreply.github.com>
2022-07-09 04:27:09 +0200

    Separate plot utilies from main code and refactor the code a little

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,16 @@
 mod naive_fourier_transform;
+mod plot;
 mod utilities;
-use naive_fourier_transform::naive_fourier_transform;
+
 use plotlib::page::Page;
-use plotlib::repr::Plot;
-use plotlib::style::{PointMarker, PointStyle};
-use plotlib::view::ContinuousView;
+
+use naive_fourier_transform::naive_fourier_transform;
+use plot::view_from_data;
 use std::f64::consts::PI;
 
 const RED: &'static str = "#DD3355";
 const BLUE: &'static str = "#4287f5";
 const GREEN: &'static str = "#34eb56";
-
-fn partial_plot(data: &[f64], amount_to_plot: usize, color: &'static str) -> Plot {
-    let plot_points: Vec<(f64, f64)> = data[0..amount_to_plot]
-        .iter()
-        .enumerate()
-        .map(|(index, value)| (index as f64, *value as f64))
-        .collect();
-    Plot::new(plot_points).point_style(
-        PointStyle::new()
-            .marker(PointMarker::Circle)
-            .colour(color)
-            .size(1.5),
-    )
-}
 
 fn generate_sin_wave(range: usize, harmonic: i16) -> Vec<f64> {
     let mut v = vec![];
@@ -47,44 +34,30 @@ fn main() {
     let sinwave = generate_sin_wave(plot_size, 3);
 
     let frequencies = naive_fourier_transform(&sinwave);
-
     let cos_frequencies = frequencies
         .iter()
         .map(|z| 2.0 * z.re / plot_size as f64)
         .collect::<Vec<f64>>();
-
     let sin_frequencies = frequencies
         .iter()
         .map(|z| 2.0 * z.im / plot_size as f64)
         .collect::<Vec<f64>>();
 
-    let plot_amplitudes = partial_plot(&sinwave, plot_size, RED);
+    let view_amplitudes = view_from_data(&sinwave, plot_size, RED);
+    let view_sin_frequencies = view_from_data(&sin_frequencies, plot_size, BLUE);
+    let view_cos_frequencies = view_from_data(&cos_frequencies, plot_size, GREEN);
 
-    let plot_sin_frequencies = partial_plot(&sin_frequencies, plot_size, BLUE);
-    let plot_cos_frequencies = partial_plot(&cos_frequencies, plot_size, GREEN);
-    let view = ContinuousView::new()
-        .add(plot_amplitudes)
-        .x_range(0.0, plot_size as f64)
-        .y_range(-1.0, 1.0);
-
-    let view2 = ContinuousView::new()
-        .add(plot_sin_frequencies)
-        .x_range(0.0, plot_size as f64)
-        .y_range(-1.0, 1.0);
-
-    let view3 = ContinuousView::new()
-        .add(plot_cos_frequencies)
-        .x_range(0.0, plot_size as f64)
-        .y_range(-1.0, 1.0);
-
-    Page::single(&view)
+    Page::single(&view_amplitudes)
         .save("plots/amplitudes_plot.svg")
         .unwrap();
-    Page::single(&view2)
+
+    Page::single(&view_sin_frequencies)
         .save("plots/sin_frequencies.svg")
         .unwrap();
-    Page::single(&view3)
+
+    Page::single(&view_cos_frequencies)
         .save("plots/cos_frequencies.svg")
         .unwrap();
+
     println!("Done !");
 }
---
+++ src/plot.rs
@@ -0,0 +1,29 @@
+use plotlib::{
+    repr::Plot,
+    style::{PointMarker, PointStyle},
+    view::ContinuousView,
+};
+
+fn partial_plot(data: &[f64], amount_to_plot: usize, color: &'static str) -> Plot {
+    let plot_points: Vec<(f64, f64)> = data[0..amount_to_plot]
+        .iter()
+        .enumerate()
+        .map(|(index, value)| (index as f64, *value as f64))
+        .collect();
+    Plot::new(plot_points).point_style(
+        PointStyle::new()
+            .marker(PointMarker::Circle)
+            .colour(color)
+            .size(1.5),
+    )
+}
+fn create_view(plot: Plot, plot_size: usize) -> ContinuousView {
+    ContinuousView::new()
+        .add(plot)
+        .x_range(0.0, plot_size as f64)
+        .y_range(-1.0, 1.0)
+}
+
+pub fn view_from_data(data: &[f64], plot_size: usize, color: &'static str) -> ContinuousView {
+    create_view(partial_plot(data, plot_size, color), plot_size)
+}
